b9aa54f3ffe44eeb412dcbb60e7ca1eff33d89ce
Alexandra Tinjacá <71520459+Epifis@users.noreply.github.com>
2023-09-13 15:58:33 -0500

    Update TiendaDeProductos.java
    Con evaluación de condicionales

--- TiendaDeProductos.java
+++ TiendaDeProductos.java
@@ -62,7 +62,110 @@ public class TiendaDeProductos {
 
         //Evaluación de expresiones relacionales
         
+        /*
+        En el método main implemente la evaluación de las expresiones relacionales:
+            tipo== DROGUERIA
+            cantidadBodega > 0
+            totalProductosVendidos > 0
+            cantidadBodega <= cantidadMinima
+        */
         
+        System.out.println("Evaluación: ");
+        System.out.println(" Tipo == drogueria");
+        System.out.print("  Libreta ");
+        if(libreta.getTipo()== 2)
+            System.out.println("es de dorguería");
+        else
+            System.out.println("no es de tipo dorgería");
+        System.out.print("  Leche ");
+        if(leche.getTipo()== 2)
+            System.out.println("es de dorguería");
+        else
+            System.out.println("no es de tipo dorgería");
+        System.out.print("  Libreta ");
+        if(leche.getTipo()== 2)
+            System.out.println("es de dorguería");
+        else
+            System.out.println("no es de tipo dorgería");
+        System.out.print("  Jabon ");
+        if(jabon.getTipo()== 2)
+            System.out.println("es de dorguería");
+        else
+            System.out.println("no es de tipo dorgería");
+        System.out.print("  Aspirina ");
+        if(aspirina.getTipo()== 2)
+            System.out.println("es de dorguería");
+        else
+            System.out.println("no es de tipo dorgería");
+        
+        //Cantidad bodega
+        System.out.println("CantidadBodega>0");
+        System.out.print("  Libreta: ");
+        if(libreta.getCantActual() > 0)
+            System.out.println("Hay productos en bodega");
+        else
+            System.out.println("No hay productos en bodega");
+        System.out.print("  Leche:  ");
+        if(leche.getCantActual() > 0)
+            System.out.println("Hay productos en bodega");
+        else
+            System.out.println("No hay productos en bodega");
+        System.out.print("  Jabon: ");
+        if(jabon.getCantActual() > 0)
+            System.out.println("Hay productos en bodega");
+        else
+            System.out.println("No hay productos en bodega");
+        System.out.print("  Aspirina: ");
+        if(aspirina.getCantActual() > 0)
+            System.out.println("Hay productos en bodega");
+        else
+            System.out.println("No hay productos en bodega");
+        
+        //Productos vendidos > 0
+        System.out.println("totalProductosVendidos > 0");
+        System.out.print("  Libreta: ");
+        if(libreta.getCantVendida() > 0)
+            System.out.println("Se vendieron productos");
+        else
+            System.out.println("No se vendieron productos");
+        System.out.print("  Leche:  ");
+        if(leche.getCantVendida() > 0)
+            System.out.println("Se vendieron productos");
+        else
+            System.out.println("No se vendieron productos");
+        System.out.print("  Jabon: ");
+        if(jabon.getCantVendida() > 0)
+            System.out.println("Se vendieron productos");
+        else
+            System.out.println("No se vendieron productos");
+        System.out.print("  Aspirina: ");
+        if(aspirina.getCantVendida() > 0)
+            System.out.println("Se vendieron productos");
+        else
+            System.out.println("No se vendieron productos");
+        
+        //cantidadBodega <= cantidadMinima
+        System.out.println("cantidadBodega <= cantidadMinima");
+        System.out.print("  Libreta: ");
+        if(libreta.getCantActual() <= libreta.getCantMinima())
+            System.out.println("Hay que comprar productos");
+        else
+            System.out.println("No hay que comprar productos");
+        System.out.print("  Leche:  ");
+        if(leche.getCantActual() <= leche.getCantMinima())
+            System.out.println("Hay que comprar productos");
+        else
+            System.out.println("No hay que comprar productos");
+        System.out.print("  Jabon: ");
+        if(jabon.getCantActual() <= jabon.getCantMinima())
+            System.out.println("Hay que comprar productos");
+        else
+            System.out.println("No hay que comprar productos");
+        System.out.print("  Aspirina: ");
+        if(aspirina.getCantActual() <= aspirina.getCantMinima())
+            System.out.println("Hay que comprar productos");
+        else
+            System.out.println("No hay que comprar productos");
     }
     
 }
